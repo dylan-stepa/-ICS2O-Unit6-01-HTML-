854e588174ccb14937bea0c96f80faa5bd79cd49
dylanstepa <dylan.stepa@ocsbstudent.ca>
2020-12-17 15:13:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+<p id="demo"></p>
+
+<script>
+  document.getElementById("demo").innerHTML =
+  Math.floor(Math.random() * 6);
+  </script>
+
+  <button onclick="myFunction()">click it</button>
+
+<script>
+function myFunction() {
+  alert("You win!");
+}
+</script>
